5434d231032f11ea17504d8fb939d37a34be1108
Tasmim Rahman <tasmimrahman2000@gmail.com>
2022-09-29 03:44:39 +0600

    header added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-name'>
+            <h1 className='header-css'>My Sports Club</h1>
+            <h2>Select today's exercise</h2>
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
